f1c1fd0af5c3270990b5755abefcf88b17dba224
Oscar Anyiam <oscaranyiam94@gmail.com>
2021-11-23 15:31:27 +0100

    finished weight card

--- lib/constants/color_constants.dart
+++ lib/constants/color_constants.dart
@@ -6,9 +6,17 @@ const KInactiveCardColor = Color(0xFF111328);
 const KBottomContainerColor = Color(0xFFEB1555);
 const KCardFontColor = Color(0xFF8D8E98);
 const KPrimaryColor = Color(0xFF0A0E21);
+
+//slider color
 const KScafoldBgColor = Color(0xFF0A0E21);
 const KSliderinactiveColor = Color(0xFF8D8E98);
+const kSliderOverlayColor = Color(0x29EB1555);
 const kWhite = Colors.white70;
+const kSliderWhite = Colors.white;
 
+//Text style constants
 const KLabelTextStyle = TextStyle(fontSize: 18, color: kWhite);
 const KNumberTextStyle = TextStyle(fontSize: 50.0, fontWeight: FontWeight.w900);
+
+//button constants
+const KRoundButtonColor = Color(0xFF4C45FE);
